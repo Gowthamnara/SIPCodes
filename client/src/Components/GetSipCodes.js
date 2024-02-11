f530c84479b524610ee1faf21d7b39628fb6063c
N <2078174@cognizant.com>
2024-02-11 21:48:14 +0530

    Changes 1

---
+++ client/src/Components/GetSipCodes.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export default async function GetSipCodes(convid){
+    try{
+
+        const [sipcodes,setSipCodes]= useState([])
+        const {convid}=useParams()
+        console.log(convid)
+
+        async function getsipcode(){
+            try{
+              const sipcodes=await axios.get('http://localhost:3001/getsipcodes/'+convid);
+              console.log(sipcodes)
+              setSipCodes(sipcodes.data)
+              return sipcodes.data
+            }
+            catch(err){
+              console.log(err)
+            }
+          }
+
+
+        <div>
+        <button onClick={getsipcode}>Refresh</button>
+        {/* {sipcodes.map((name,index)=>{return <option key={index} value={name}>{name}</option>})} */}
+        {sipcodes.map((value,index)=>{return <option key={index}>{value.Method}{value.Description?"-"+value.Description:""}</option>})}
+        </div>
+      }
+      catch(err){
+        console.log(err)
+      }
+}
