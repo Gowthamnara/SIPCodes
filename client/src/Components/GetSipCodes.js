e5fe26f1a5acd43ececb87a35e2a2c701e2e3b45
Gowthamnara <64727756+Gowthamnara@users.noreply.github.com>
2024-02-16 12:40:03 +0530

    Update GetSipCodes.js

--- client/src/Components/GetSipCodes.js
+++ client/src/Components/GetSipCodes.js
@@ -2,9 +2,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
 
-export default async function GetSipCodes(){
-    try{
-
+export default function GetSipCodes(){
         const [sipcodes,setSipCodes]= useState([])
         const {convid}=useParams()
         console.log(convid)
@@ -21,14 +19,11 @@ export default async function GetSipCodes(){
             }
           }
 
-
+      return (
         <div>
         <button onClick={getsipcode}>Refresh</button>
         {/* {sipcodes.map((name,index)=>{return <option key={index} value={name}>{name}</option>})} */}
         {sipcodes.map((value,index)=>{return <option key={index}>{value.Method}{value.Description?"-"+value.Description:""}</option>})}
         </div>
-      }
-      catch(err){
-        console.log(err)
-      }
+      )
 }
